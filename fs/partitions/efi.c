9ba6e91ac7201a89abbb779357a36eb37419216a
Vlad Starodubtsev <v.starodubtsev@ndmsystems.com>
2016-01-19 20:06:46 +0300

    852-efi_sector_size.patch

--- fs/partitions/efi.c
+++ fs/partitions/efi.c
@@ -135,6 +135,15 @@ efi_crc32(const void *buf, unsigned long len)
 }
 
 /**
+ * div64_u64 - unsigned 64bit divide with 64bit divisor
+ */
+static inline u64 div_u64(u64 dividend, u32 divisor)
+{
+	u32 remainder = do_div(dividend, divisor);
+	return dividend;
+}
+
+/**
  * last_lba(): return number of last logical block of device
  * @bdev: block device
  * 
@@ -148,7 +157,8 @@ last_lba(struct block_device *bdev)
 {
 	if (!bdev || !bdev->bd_inode)
 		return 0;
-	return (bdev->bd_inode->i_size >> 9) - 1ULL;
+	return div_u64(bdev->bd_inode->i_size,
+		bdev_hardsect_size(bdev)) - 1ULL;
 }
 
 static inline int
@@ -195,16 +205,17 @@ static size_t
 read_lba(struct block_device *bdev, u64 lba, u8 * buffer, size_t count)
 {
 	size_t totalreadcount = 0;
-
-	if (!bdev || !buffer || lba > last_lba(bdev))
+	sector_t n = lba * (bdev_hardsect_size(bdev) / 512);
+	
+	if (!bdev || !buffer || lba > last_lba(bdev)) 
                 return 0;
 
 	while (count) {
 		int copied = 512;
 		Sector sect;
-		unsigned char *data = read_dev_sector(bdev, lba++, &sect);
-		if (!data)
-			break;
+		unsigned char *data = read_dev_sector(bdev, n++, &sect);
+		if (!data) 
+			break;	
 		if (copied > count)
 			copied = count;
 		memcpy(buffer, data, copied);
@@ -264,15 +275,16 @@ static gpt_header *
 alloc_read_gpt_header(struct block_device *bdev, u64 lba)
 {
 	gpt_header *gpt;
+	unsigned ssz = bdev_hardsect_size(bdev);
 	if (!bdev)
 		return NULL;
 
-	gpt = kzalloc(sizeof (gpt_header), GFP_KERNEL);
+	gpt = kzalloc(ssz, GFP_KERNEL);
 	if (!gpt)
 		return NULL;
 
 	if (read_lba(bdev, lba, (u8 *) gpt,
-		     sizeof (gpt_header)) < sizeof (gpt_header)) {
+		     ssz) < ssz) {
 		kfree(gpt);
                 gpt=NULL;
 		return NULL;
@@ -609,6 +621,7 @@ efi_partition(struct parsed_partitions *state, struct block_device *bdev)
 	gpt_header *gpt = NULL;
 	gpt_entry *ptes = NULL;
 	u32 i;
+	unsigned ssz = bdev_hardsect_size(bdev) / 512;
 
 	if (!find_valid_gpt(bdev, &gpt, &ptes) || !gpt || !ptes) {
 		kfree(gpt);
@@ -619,14 +632,14 @@ efi_partition(struct parsed_partitions *state, struct block_device *bdev)
 	Dprintk("GUID Partition Table is valid!  Yea!\n");
 
 	for (i = 0; i < le32_to_cpu(gpt->num_partition_entries) && i < state->limit-1; i++) {
+		u64 start = le64_to_cpu(ptes[i].starting_lba);
+		u64 size = le64_to_cpu(ptes[i].ending_lba) -
+		le64_to_cpu(ptes[i].starting_lba) + 1ULL;
+
 		if (!is_pte_valid(&ptes[i], last_lba(bdev)))
 			continue;
 
-		put_partition(state, i+1, le64_to_cpu(ptes[i].starting_lba),
-				 (le64_to_cpu(ptes[i].ending_lba) -
-                                  le64_to_cpu(ptes[i].starting_lba) +
-				  1ULL));
-
+		put_partition(state, i+1, start * ssz, size * ssz);
 		/* If this is a RAID volume, tell md */
 		if (!efi_guidcmp(ptes[i].partition_type_guid,
 				 PARTITION_LINUX_RAID_GUID))
