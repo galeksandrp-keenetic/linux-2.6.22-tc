44ba266eb32470779b8e1229d20424d9df9940fe
Ilya Ponetaev <i.ponetaev@ndmsystems.com>
2016-03-23 15:56:08 +0300

    Fix ntc shaper hooks (case 7584)

--- net/core/ntc_shaper_hooks.c
+++ net/core/ntc_shaper_hooks.c
@@ -10,3 +10,5 @@ EXPORT_SYMBOL(ntc_shaper_ingress_hook);
 ntc_shaper_hook_fn *ntc_shaper_egress_hook = NULL;
 EXPORT_SYMBOL(ntc_shaper_egress_hook);
 
+int (*ntc_shaper_check_ip_and_mac)(uint32_t ipaddr, uint8_t * mac) = NULL;
+EXPORT_SYMBOL(ntc_shaper_check_ip_and_mac);
