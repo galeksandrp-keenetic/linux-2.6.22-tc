65ca14e398e332aa607aa9cecb77f536a355fc17
Eugene Yudin <e.yudin@ndmsystems.com>
2016-07-28 18:39:07 +0300

    Revert "Bugfix and update signalfd function."
    This reverts commit 2609ecc3e83a5d6d95149a5c16ac43f7a5511f2a.

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -395,7 +395,7 @@ struct sighand_struct {
 	atomic_t		count;
 	struct k_sigaction	action[_NSIG];
 	spinlock_t		siglock;
-	wait_queue_head_t	signalfd_wqh;
+	struct list_head        signalfd_list;
 };
 
 struct pacct_struct {
